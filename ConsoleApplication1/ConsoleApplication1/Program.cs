eb68ba331f07bae29fa91261b0ec2dfd58a08d52
mas205 <martinhargain@gmail.com>
2018-03-19 10:16:47 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televisor r = new Televisor("Panasonic", "KCV-25", Televisor.EnumTipoPantalla.LCD , true, Televisor.EnumEstado.APAGADO, 50, 50, 50);
+            Console.ReadLine();
+            Console.WriteLine(r);
+            r.Brillo = Convert.ToInt32(Console.ReadLine());            
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApplication1/Dominio/Televisor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Televisor
+    {
+        #region Atributos
+        private string marca;
+        private string modelo;
+        public enum EnumTipoPantalla { LED = 1, LCD, PLASMA, TRC }
+        private bool smart;
+        public enum EnumEstado { PRENDIDO, APAGADO, STANDBY }
+        private int brillo;
+        private int contraste;
+        private int volumen;
+        #endregion
+
+        #region Properties
+        public string Marca
+        {
+            get { return marca; }
+            //set { marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            //set { modelo = value; }
+        }
+
+        private EnumTipoPantalla tipoPantalla;
+        public EnumTipoPantalla TipoPantalla
+        {
+            get { return tipoPantalla; }
+            //set { tipoPantalla = value; }
+        }
+
+        public bool Smart
+        {
+            get { return smart; }
+        }
+
+        private EnumEstado estado;
+
+        public EnumEstado Estado
+        {
+            get { return estado; }
+        }
+        public int Brillo
+        {
+            get { return brillo; }
+            set { brillo = value; }
+        }
+        public int Contraste
+        {
+            get { return contraste; }
+            set { contraste = value; }
+        }
+        public int Volumen
+        {
+            get { return volumen; }
+            set { volumen = value; }
+        }
+
+
+
+        #endregion
+
+        #region Constructor
+        public Televisor(string marca, string modelo, EnumTipoPantalla tipoPantalla, bool smart, EnumEstado estado, int brillo, int contraste, int volumen)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.tipoPantalla = tipoPantalla;
+            this.smart = smart;
+            this.estado = estado;
+            this.brillo = brillo;
+            this.contraste = contraste;
+            this.volumen = volumen;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return string.Format("Marca: {0}\nModelo: {1}\nTipo de pantalla: {2}\nSmart: {3}\nEstado: {4}\nBrillo: {5}\nContraste: {6}\nVolumen: {7}", modelo, marca, tipoPantalla, smart, estado, brillo, contraste, volumen);          
+        }
+    }
+}
